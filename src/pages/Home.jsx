4724534529a0f79f4235371f66340d184ccf2cf9
C043 <mariofragnitoph@gmail.com>
2024-06-28 15:18:58 +0200

    Fixed typo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,34 @@
 import { Component } from "react";
 import NetflixNav from "../components/NetflixNav";
-import NetflixFooter from "../components/NetflixFootter";
+import NetflixFooter from "../components/NetflixFooter";
 import { Container } from "react-bootstrap";
 import NetflixHeader from "../components/NetflixHeader";
 import NetflixGallery from "../components/NetflixGallery";
+import Profile from "../components/Profile";
 
 class Home extends Component {
+  state = {
+    home: false,
+    profile: true,
+    settings: false,
+  };
   render() {
     return (
       <>
         <NetflixNav />
-        <Container>
-          <NetflixHeader />
-          <NetflixGallery search="Harry Potter" />
-          <NetflixGallery search="Star Wars" />
-          <NetflixGallery search="Dahmer" />
-        </Container>
+        {this.state.home && (
+          <Container>
+            <NetflixHeader />
+            <NetflixGallery search="Harry Potter" />
+            <NetflixGallery search="Star Wars" />
+            <NetflixGallery search="Dahmer" />
+          </Container>
+        )}
+        {this.state.profile && (
+          <Container>
+            <Profile />
+          </Container>
+        )}
         <NetflixFooter />
       </>
     );
