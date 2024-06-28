a57b3d6721e233db53ac0d5f63086bcc85299bcd
C043 <mariofragnitoph@gmail.com>
2024-06-28 11:47:15 +0200

    Adds header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import { Component } from "react";
 import NetflixNav from "../components/NetflixNav";
 import NetflixFooter from "../components/NetflixFootter";
+import { Container } from "react-bootstrap";
+import NetflixHeader from "../components/NetflixHeader";
 
 class Home extends Component {
   render() {
     return (
       <>
         <NetflixNav />;
+        <Container>
+          <NetflixHeader />
+        </Container>
         <NetflixFooter />
       </>
     );
