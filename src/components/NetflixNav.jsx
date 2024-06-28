75fa8dd1e37ce9580a12caffc8495f42e92f18c3
C043 <mariofragnitoph@gmail.com>
2024-06-28 15:51:56 +0200

    Adds active to the navbar

--- src/components/NetflixNav.jsx
+++ src/components/NetflixNav.jsx
@@ -11,7 +11,9 @@ const NetflixNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home" active>
+              Home
+            </Nav.Link>
             <Nav.Link href="#link">Tv Shows</Nav.Link>
             <Nav.Link href="#link">Movies</Nav.Link>
             <Nav.Link href="#link">Recently Added</Nav.Link>
